48f89001a4fba3bb4cfe9c7090b5e9193958fb76
gitjoey <31334632+codejoey@users.noreply.github.com>
2021-10-10 11:20:39 +0800

    prepared ext files

---
+++ firebase-init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhzBcy71ooyvYNIyCYVsCIM97M4iOO0W8",
+  authDomain: "coupon-finder-668bf.firebaseapp.com",
+  projectId: "coupon-finder-668bf",
+  storageBucket: "coupon-finder-668bf.appspot.com",
+  messagingSenderId: "254999250241",
+  appId: "1:254999250241:web:5a617106bfa38cd6728dc0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
