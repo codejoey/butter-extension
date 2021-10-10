f1fac10166151749953eb903b6892d3d92c084b0
gitjoey <31334632+codejoey@users.noreply.github.com>
2021-10-10 19:46:48 +0800

    converted price pull functions into python function file

--- cart pull API/price.py
+++ cart pull API/price.py
@@ -2,6 +2,31 @@ import selenium
 from selenium import webdriver
 import requests
 
+#This pulls price of all items in cart as subtotal
+def cartFromUrl(url):
+    # The requests.Response() Object contains the server's response to the HTTP request.
+    #response = requests.get(url)
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    #This pulls price of all items in cart as subtotal
+    cartSubtotal = driver.find_element_by_id("sc-subtotal-amount-activecart")
+
+    return(cartSubtotal)
+
+#This pulls price of item from the item's store page
+def itemFromUrl(url):
+    # The requests.Response() Object contains the server's response to the HTTP request.
+    #response = requests.get(url)
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    #This pulls price of item from the item's store page
+    itemPricePage = driver.find_element_by_id("price_inside_buybox")
+    
+    return(itemPricePage)
+
+"""
 driver = webdriver.Chrome()
 driver.get('https://www.amazon.com/gp/product/B0983M5SHW?pf_rd_r=HHGPWKKVW24FPZGG8K1Z&pf_rd_p=8fe9b1d0-f378-4356-8bb8-cada7525eadd&pd_rd_r=1d84c989-d684-4e83-be9c-22359661f863&pd_rd_w=CrYwg&pd_rd_wg=qgFtM&ref_=pd_gw_unk')
 
@@ -15,21 +40,4 @@ itemPricePage = driver.find_element_by_id("price_inside_buybox")
 
 #This pulls price of all items in cart as subtotal
 cartSubtotal = driver.find_element_by_id("sc-subtotal-amount-activecart")
-
-def cartFromUrl(url):
-    # read images from URL
-    response = requests.get(url)
-    img = Image.open(BytesIO(response.content))
-    # convert the image to rgb
-    image = img.convert('RGB')
-     
-    # resize the image to 100 x 100
-    image = image.resize((100,100))
-     
-    detected_colors =[]
-    for x in range(image.width):
-        for y in range(image.height):
-            detected_colors.append(closest_colour(image.getpixel((x,y))))
-    Series_Colors = pd.Series(detected_colors)
-    output=Series_Colors.value_counts()/len(Series_Colors)
-    return(output.head(n).to_dict())
+"""
